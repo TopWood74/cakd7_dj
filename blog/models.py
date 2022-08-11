eb14ef47489b218dd1e2912db8773992933fa961
TopWood74 <topwood@hanmail.net>
2022-08-11 16:35:54 +0900

    댓글 폼으로 포스트 상세 페이지에서 댓글 작성하기

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from venv import create
 from django.db import models
 from django.contrib.auth.models import User
 from markdownx.models import MarkdownxField
@@ -57,3 +58,17 @@ class Post(models.Model):
  
     def get_content_markdown(self):
         return markdown(self.content)
+
+class Comment(models.Model):
+    post  = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
+    
+    def get_absolute_url(self):
+        return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+    
