3813dbc5543c4a1aa6e0470fbb1322006c82a7dd
TopWood74 <topwood@hanmail.net>
2022-08-12 11:59:31 +0900

    검색 기능 구현하기

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ from .models import Post, Category, Tag, Comment
 from .forms import CommentForm
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
+from django.db.models import Q
+   
 
 class CommentUpdate(LoginRequiredMixin, UpdateView):
     model = Comment
@@ -20,6 +22,7 @@ class CommentUpdate(LoginRequiredMixin, UpdateView):
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
@@ -110,6 +113,22 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
 
         return response
 
+class PostSearch(PostList):
+    paginate_by = None
+
+    def get_queryset(self):
+        q = self.kwargs['q']
+        post_list = Post.objects.filter(
+            Q(title__contains=q) | Q(tags__name__contains=q)
+        ).distinct()
+        return post_list
+
+    def get_context_data(self, **kwargs):
+        context = super(PostSearch, self).get_context_data()
+        q = self.kwargs['q']
+        context["search_info"] = f'Search: {q} ({self.get_queryset().count()})'
+
+        return context
 
 def category_page(request, slug):
     if slug == 'no_category':
